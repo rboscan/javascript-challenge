0f80eada8df78004fc064fc7ce21720abb6ef276
rboscan <51136553+rboscan@users.noreply.github.com>
2019-11-09 08:10:35 -0500

    Finalized Version w/ Reset Button

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,16 +1,55 @@
 console.log(data);
 var tbody = d3.select("tbody");
 
+let reset = d3.select("#reset-btn");
+
 data.forEach((ufosighting) => {
-    
-    console.log(ufosighting);
-    
+
     const tr = tbody.append("tr");
-    
-    Object.entries(ufosighting).forEach(([key,value]) => {
-    
-        console.log(`${key}: ${value}`);
-    
+
+    Object.entries(ufosighting).forEach(([key, value]) => {
+
         tr.append("td").text(value);
+    });
+});
+
+reset.on("click", function () {
+    
+    data.forEach((ufosighting) => {
+
+        const tr = tbody.append("tr");
+
+        Object.entries(ufosighting).forEach(([key, value]) => {
+
+            tr.append("td").text(value);
+        });
+    });
+});
+
+let ufo_button = d3.select("#filter-btn")
+
+ufo_button.on("click", function () {
+
+    let inputElement = d3.select(".form-control");
+
+    let inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    let filter = data.filter(sighting => sighting.datetime === inputValue);
+
+    console.log(filter);
+
+    tbody.html("");
+
+
+    filter.forEach((ufosighting) => {
+
+        const tr = tbody.append("tr");
+
+        Object.entries(ufosighting).forEach(([key, value]) => {
+
+            tr.append("td").text(value);
+        });
     });
 });
